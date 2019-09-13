b551788f45b01cbab06a964b0fc3f66971b9b789
ameliaaraujo <amelia.araujo@cefas.co.uk>
2019-09-13 10:55:48 +0100

    ficheiro mudado

---
+++ exercicio_0.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 29 10:13:23 2015
+
+@author: aa02
+"""
+
+key = ''
+x = []
+y = []
+z = []
+
+fileout=open('ex0.out','w')
+fileout.write('"x"'',''"y"'',''"z"'+'\n')
+
+for line in open('ex0.in'):
+    
+    columns = line.strip().split()
+       
+    if ':EndHeader' in columns[0]:
+        key = ':EndHeader'    
+   
+    else:  
+        if key == ':EndHeader' :
+            x = columns[0]            
+            y = columns[1]
+            z = columns[2]
+            fileout.write(x + ',' + y +  ','  + z+'\n') 
+            
+  
+fileout.close()
+
+print x
+print y
+print z
+
